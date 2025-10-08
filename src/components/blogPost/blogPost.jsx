13c626d5d920bc3b978ca3168fde9091441b62c7
naveenstheninjastudio <naveens@theninjastudio.com>
2025-10-08 14:21:09 +0530

    text alignment fix

--- src/components/blogPost/blogPost.jsx
+++ src/components/blogPost/blogPost.jsx
@@ -1,7 +1,7 @@
 export default function BlogPost(props) {
   return (
     <div className="flex flex-col p-1 justify-center items-center mb-5 w-[90vw]">
-      <div className="flex text-xl p-1 justify-center text-white font-semibold border-2 bg-[#121212] border-white w-full rounded-xl">
+      <div className="flex text-xl p-1 justify-center text-center text-white font-semibold border-2 bg-[#121212] border-white w-full rounded-xl">
         {props.title}
       </div>
       <div className="font-light p-2">{props.body}</div>
